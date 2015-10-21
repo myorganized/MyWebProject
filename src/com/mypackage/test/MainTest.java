d109963369fecb4c1d44a2725501ff2112b118bc
mylawrence00 <lawrence@gwtsz.com>
2015-10-21 10:19:45 +0800

    commi test123

--- src/com/mypackage/test/MainTest.java
+++ src/com/mypackage/test/MainTest.java
@@ -3,8 +3,8 @@ package com.mypackage.test;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-
+		// TODO Auto-generated method stub
+		System.out.println("lakala test  234234a");
 	}
 
 }
